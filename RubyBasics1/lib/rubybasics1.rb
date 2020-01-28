dbf1c389a161f8aa87fd1b3d48fc5f75034d687e
Corey Schneider <cschne11@uncc.edu>
2020-01-28 19:00:06 +0000

    RubyBasics1 - Part 1

---
+++ RubyBasics1/lib/rubybasics1.rb
@@ -0,0 +1,18 @@
+# Lab 1
+# Part I
+def sum arr
+  if(arr.length == 0)
+    return 0
+  end
+   return arr.inject(:+)
+end
+
+# Part II
+def max_2_sum arr
+  # YOUR CODE HERE
+end
+
+# Part III
+def sum_to_n? arr, n
+  # YOUR CODE HERE
+end
