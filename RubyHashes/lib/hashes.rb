94d5bdad434d4434ee71bc33327895cf50d452e8
Corey Schneider <cschne11@uncc.edu>
2020-02-04 06:03:41 +0000

    RubyHashes - Part 1

--- RubyHashes/lib/hashes.rb
+++ RubyHashes/lib/hashes.rb
@@ -1,12 +1,30 @@
 # RubyHashes
+
 # Part I
 def array_2_hash emails, contacts
-    # YOUR CODE HERE
+    i = 0
+    if (emails.any?) # skip if no emails
+        contacts.each do |key, value|
+            puts "key: #{key}, value: #{value}"
+            contacts[key] = emails[i]
+            i += 1
+            puts "contacts #{contacts}"
+        end
+    end
+    return contacts
 end
+
 
 # Part II
 def array2d_2_hash contact_info, contacts
-    # YOUR CODE HERE
+    i = 0
+    if (contact_info[0].any?)
+        contacts.each do |key, value|
+            contacts[key] = {"email" => contact_info[i][0], "phone" => contact_info[i][1]}
+            i += 1
+        end
+    end
+    return contacts
 end
 
 # Part III
